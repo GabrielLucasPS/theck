6da2d13f14114891b3daa36240f835f8055df95b
Gabriel Lucas <gabrielglpsi@gmail.com>
2023-11-27 23:20:54 -0300

    erro do async no navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,9 @@
-import { authOptions } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
-import Link from 'next/link';
 import { Button, buttonVariants } from './ui/button';
-import Roleteste from './roleteste';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
-const Navbar = async () => {
-    const session = await getServerSession(authOptions);
-
+const Navbar = () => {
+    const { data: session } = useSession()
     return (
         <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full h-[65px] z-10 top-0'>
             <div className='container flex items-center justify-between'>
